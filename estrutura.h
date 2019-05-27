b715974353c7963467a23b556cd7c88a2981e596
gabrielleite751 <42241006+gabrielleite751@users.noreply.github.com>
2019-05-27 10:22:46 -0300

    Add files via upload

---
+++ estrutura.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER 100
+
+struct lista{
+	char *nome;
+	char email;
+	int tell;
+	int matricula;
+	char setor;
+	struct lista *next;
+};
+
+struct lista *inicia_dados(char *nome, char email, int tell, int matricula, char setor);
+struct lista *insere_dados(struct lista *dados, char *nome, char email, int tell, int matricula, char setor);
+void exibe_dados(struct lista *dados);
+void busca_dados(struct lista *dados, char *key);
+struct lista *deleta_dados(struct lista *dados);
+int checa_vazio(struct lista *dados);
+
+void insere(void);
+void busca(void);
+void exibe(void);
+void deleta(void);
+
+struct lista *principal = NULL;
